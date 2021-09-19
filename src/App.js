c4c6dea61133e6947f078592c044f2caf0eb5e3d
Muskan Hurria <muskanhurria@gmail.com>
2021-09-20 00:02:18 +0530

    firebase used

--- src/App.js
+++ src/App.js
@@ -1,45 +1,92 @@
 import React from "react";
 import Cart from './Cart';
 import NavBar from './NavBar';
+// import * as firebase from 'firebase';
+// import firebase from 'firebase/app';   
+import firebase from 'firebase/app';
+
+import '@firebase/firestore'
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
-        products: [
-            {
-              price: 1000,
-              title: 'Watch',
-              qty: 1,
-              img: '',
-              id: 1 
-            },
-            {
-              price: 1000,
-              title: 'Mobile Phone',
-              qty: 10,
-              img: '', 
-              id: 2
-            },
-            {
-              price: 1000,
-              title: 'Laptop',
-              qty: 4,
-              img: '',
-              id: 3
-            }
-        ] 
+         products: [],
+         loading: true
     }
+
+    this.db = firebase.firestore()
 }
+
+  componentDidMount() {
+    // firebase
+    // .firestore()
+    // .collection('products')
+    // .get()           //it returns a promise
+    // .then((snapshot) => {
+    //   console.log(snapshot);
+
+    //   snapshot.docs.map((doc) => {
+    //     console.log(doc.data);
+    //   });
+
+    //   const products = snapshot.docs.map((doc) => {
+    //     const data = doc.data();
+
+    //     data['id'] = doc.id;
+    //     return data();
+    //   });
+
+    //   this.setState({
+    //     products,          //same as products: products
+    //     loading: false
+    //   })
+    // })
+
+    this.db
+      .collection('products')
+      .onSnapshot((snapshot) => {       //onsnapshot is fird automatically whenever change is made in database
+        console.log(snapshot);
+
+        snapshot.docs.map((doc) => {
+          console.log(doc.data);
+        });
+
+        const products = snapshot.docs.map((doc) => {
+          const data = doc.data();
+
+          data['id'] = doc.id;
+          return data;
+        });
+
+        this.setState({
+          products,          //same as products: products
+          loading: false
+        })
+      
+      })
+        
+  }
 
   handleIncreaseQuantity = (product) => {
       const { products } = this.state;
       const index = products.indexOf(product);
 
-      products[index].qty += 1;
-      this.setState({
-          products: products        //or simply right products both mean same
-      });
+      // products[index].qty += 1;
+      // this.setState({
+      //     products: products        //or simply right products both mean same
+      // });
+
+      const docRef = this.db.collection("products").doc(products[index].id);
+
+      docRef
+        .update({ qty: products[index].qty + 1 })
+        .then(() => {
+          console.log("Document updated sucessfully");
+        })
+        .catch(error => {
+          console.log(error);
+        });
 
   }
 
@@ -50,20 +97,43 @@ class App extends React.Component {
       if(products[index].qty == 0){
           return;
       }
-      products[index].qty -= 1;
-      this.setState({
-          products: products        //or simply right products both mean same
-      });
+
+      // products[index].qty -= 1;
+      // this.setState({
+      //     products: products        //or simply right products both mean same
+      // });
+
+      const docRef = this.db.collection("products").doc(products[index].id);
+
+      docRef
+        .update({ qty: products[index].qty - 1 })
+        .then(() => {
+          console.log("Document updated sucessfully");
+        })
+        .catch(error => {
+          console.log(error);
+        });
 
   }
 
   handleDeleteProduct = (id) => {
-      const { products } = this.state;
+    const { products } = this.state;
 
-      const items = products.filter((item) => item.id !== id);     //this will return an array
-      this.setState({
-          products: items
+    const docRef = this.db.collection("products").doc(id);
+
+    docRef
+      .delete()
+      .then(() => {
+        console.log("Deleted sucessfully");
+      })
+      .catch(err => {
+        console.log(err);
       });
+
+    // const items = products.filter((item) => item.id !== id);     //this will return an array
+    // this.setState({
+    //     products: items
+    // });
   }
 
   getCartCount = () =>{
@@ -84,18 +154,36 @@ class App extends React.Component {
 
     products.map((product) => {
       cartTotal = cartTotal + product.qty * product.price;
+      console.log('cartTotal: ', cartTotal);
     });
 
     return cartTotal;
   }
 
+  addProduct = () => {
+    this.db
+    .collection('products')
+    .add({
+      img: '',
+      price: 900,
+      qty: 3,
+      title: 'washing machine'
+    })
+    .then((docRef) => {
+      console.log('Product has been added', docRef);
+    })
+
+    .catch((err) => {
+      console.log('Error: ', err);
+    })
+  }
+
   render(){
-    const { products } = this.state;
+    const { products,loading } = this.state;
     return (
       <div className="App">
-        <NavBar 
-          count={this.getCartCount()}
-        />
+        <NavBar count={this.getCartCount()} />
+        {/* <button onClick={this.addProduct} style={{padding:20, fontSize: 20}}> Add a Product </button> */}
         <Cart 
           products= {products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
@@ -103,6 +191,7 @@ class App extends React.Component {
           onHandleDeleteProduct={this.handleDeleteProduct}
         />
 
+        {loading && <h1> Loading Products</h1>}
         <div style= { {padding: 10 ,fontSize: 20} }>
           TOTAL: {this.getCartTotal()} 
         </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import * as firebase from 'firebase';
+// import firebase from 'firebase/app';
+import firebase from 'firebase/app';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA-4byO2UXwdTI8NNBK6CnpoFjrd4tba20",
+  authDomain: "cart-e1087.firebaseapp.com",
+  projectId: "cart-e1087",
+  storageBucket: "cart-e1087.appspot.com",
+  messagingSenderId: "85023467269",
+  appId: "1:85023467269:web:cabbc87bb099547dd0b96c"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+// all good ab chlega code aapka
 
 ReactDOM.render(
   <React.StrictMode>
